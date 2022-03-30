c2f5aa7d6f87675ce4a448d9964210fb03d714ca
Tunahan <tunahankarabayir0@gmail.com>
2022-03-30 14:16:08 +0300

    Message

---
+++ src/component/Comment/CommentForm.js
@@ -0,0 +1,90 @@
+import React , {useState} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Avatar, CardContent, InputAdornment, OutlinedInput } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import  Button  from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+    comment: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        allignItems: "center",
+    },
+    small: {
+        width: theme.spacing(4),
+        height: theme.spacing(4),
+    },
+    link: {
+        textDecoration: "none",
+        boxShadow: "none",
+        color: "white",
+    }
+}));
+
+function CommentForm(props) {
+    const[text,setText]=useState("");
+    const { userId, userName , postId ,setCommentRefresh} = props;
+    const classes = useStyles();
+
+    const saveComment = () =>{
+        fetch("/comments", {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                postId:postId,
+                userId:userId,
+                text:text
+            }),
+        })   
+
+        .then((res) => res.json())
+        .catch((err) => console.log(err))
+    }
+
+    const handleChange = (value) => {
+        setText(value);
+    }
+
+    const handleSubmit = () => {
+        saveComment();
+        setText("");
+        setCommentRefresh();
+    }
+
+    return (
+        <CardContent className={classes.comment}>
+            <OutlinedInput
+                id="outlined-adornment-amount"
+                multiline
+                inputProps={{ maxLength: 200 }}
+                fullWidth
+                onChange={(i) => handleChange(i.target.value)}
+                startAdornment={
+                    <InputAdornment position="start">
+                        <Link className={classes.link} to={{ pathname: '/users/' + userId }}>
+                            <Avatar aria-label="recipe" className={classes.small}>
+                                {userName.charAt(0).toUpperCase()}
+                            </Avatar>
+                        </Link>
+                    </InputAdornment>
+                }
+                endAdornment={
+                    <InputAdornment position="end">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleSubmit}
+                        >Comment</Button>
+                    </InputAdornment>
+                }
+                value={text}
+                style={{ color: "black", backgroundColor: 'white' }}
+            ></OutlinedInput>
+        </CardContent>
+    )
+}
+
+export default CommentForm;
